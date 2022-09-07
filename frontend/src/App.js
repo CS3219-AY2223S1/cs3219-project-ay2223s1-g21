e729c87791c28a9b6fdd29068688eaed02e314fa
DESHAO-IP5I\Home Admin <e0544387@u.nus.edu>
2022-09-07 20:25:21 +0800

    create login and signup pg

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,15 @@
 import {BrowserRouter as Router, Routes, Route, Navigate} from "react-router-dom";
-import SignupPage from './components/SignupPage';
-import {Box} from "@mui/material";
+import SignupPage from './pages/SignupPage/SignupPage';
 
 function App() {
     return (
         <div className="App">
-            <Box display={"flex"} flexDirection={"column"} padding={"4rem"}>
-                <Router>
-                    <Routes>
-                        <Route exact path="/" element={<Navigate replace to="/signup" />}></Route>
-                        <Route path="/signup" element={<SignupPage/>}/>
-                    </Routes>
-                </Router>
-            </Box>
+            <Router>
+                <Routes>
+                    <Route exact path="/" element={<Navigate replace to="/signup" />}></Route>
+                    <Route path="/signup" element={<SignupPage/>}/>
+                </Routes>
+            </Router>
         </div>
     );
 }
