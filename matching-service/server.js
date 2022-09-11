cbba07108d2eb09a41a227734e97634e113d3e13
Li Quan <linliquan1996@hotmail.com>
2022-09-11 23:55:30 +0800

    Add Listener for socket event matchSuccess , matchFailure and redirect to room page

--- matching-service/server.js
+++ matching-service/server.js
@@ -70,7 +70,6 @@ io.on('connection', function (socket) {
     // When a client disconnects (leaves room)
     socket.on('disconnect', (data) => {
         console.log("Disconnected");
-        var data = JSON.parse(data);
         console.log(data.email)
         fns.endInterview(data.email);
     })
