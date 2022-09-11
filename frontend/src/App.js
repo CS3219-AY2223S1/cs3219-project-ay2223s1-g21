df854efcc4951afd8a69521469e2f30748ee4dc2
Li Quan <linliquan1996@hotmail.com>
2022-09-11 21:10:39 +0800

    Correct merge conflicts

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import Home from "./pages/Home";
 import LoadingScreen from "./pages/LoadingScreen";
 import LoginPage from "./pages/LoginPage";
 import PrivateRoute from "./components/PrivateRoute";
+import MatchingPage from "./pages/Matching/Matching";
 
 function App() {
   const { isLoading } = useSelector((state) => state.authReducer);
@@ -19,7 +20,7 @@ function App() {
             path="/change_password"
             element={<PrivateRoute component={ChangePasswordPage} />}
           />
-          <Route path="/matching" element={<MatchingPage />} />
+          <Route path="/matching" element={<PrivateRoute component={MatchingPage} />} />
           <Route path="*" element={<PrivateRoute component={Home} />} />
         </Routes>
       </Router>
