9d01ce94266da193d5a503aa0c72c7458d42617a
tandeshao <e0544387@u.nus.edu>
2022-09-20 13:56:05 +0800

    add resizer and difficulty tag

--- frontend/src/pages/CollaborationPage/index.js
+++ frontend/src/pages/CollaborationPage/index.js
@@ -11,9 +11,51 @@ import {
 } from "./CollabElements";
 import EmbeddedEditor from "./EmbeddedEditor";
 import QuestionSection from "./QuestionSection";
-import Button from '@mui/material/Button';
+import Button from "@mui/material/Button";
+import { useRef } from "react";
+import { useEffect } from "react";
 
 export default function CollaborationPage() {
+  const separatorRef = useRef(null);
+  const questionRef = useRef(null);
+  const embeddedEditorRef = useRef(null);
+
+  useEffect(() => {
+    
+    const resizableEditorEle = embeddedEditorRef.current;
+    const resizerEle = separatorRef.current;
+    const editorEleStyles = window.getComputedStyle(resizableEditorEle);
+    let width = parseInt(editorEleStyles.width, 10);
+    let x = 0;
+
+    // Right resize
+    const onMouseMoveRightResize = (event) => {
+      event.preventDefault();
+      const dx = x - event.clientX;
+      x = event.clientX;
+      width = width + dx;
+   
+      resizableEditorEle.style.width = `${width}px`;
+    };
+
+    const onMouseUpRightResize = (event) => {
+      document.removeEventListener("mousemove", onMouseMoveRightResize);
+    };
+
+    const onMouseDownRightResize = (event) => {
+      x = event.clientX;
+      document.addEventListener("mousemove", onMouseMoveRightResize);
+      document.addEventListener("mouseup", onMouseUpRightResize);
+    };
+
+    // Add mouse down event listener
+    
+    resizerEle.addEventListener("mousedown", onMouseDownRightResize);
+    return () => {
+      resizerEle.removeEventListener("mousedown", onMouseDownRightResize);
+    };
+  }, [])
+
   return (
     <PgContainer>
       <HeaderContainer>
@@ -23,14 +65,18 @@ export default function CollaborationPage() {
         </MenuItems>
       </HeaderContainer>
       <ContentContainer>
-        <QuestionContainer>
+        <QuestionContainer ref={questionRef}>
           <QuestionSection />
         </QuestionContainer>
-        <MiddleSeparator />
-        <EmbeddedEditor />
+        <MiddleSeparator ref={separatorRef} />
+        <EmbeddedEditor editorRef={embeddedEditorRef} />
       </ContentContainer>
       <FooterContainer>
-        <Button variant="outlined" color="error" style={{marginRight: "30px"}}>
+        <Button
+          variant="outlined"
+          color="error"
+          style={{ marginRight: "30px" }}
+        >
           Exit Session
         </Button>
       </FooterContainer>
