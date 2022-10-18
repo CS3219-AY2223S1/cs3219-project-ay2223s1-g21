f7600fa489c0816fdd0d78348f545153364d7464
Lim Li Quan <linliquan1996@Hotmail.com>
2022-10-19 00:25:09 +0800

    Refactor Matching Service , Add Cancel Match Feature (#85)
    * Refactor Matching Service , Add Cancel Match Feature
    
    * Update all status in verifyToken to be UNAUTHORIZED
    
    * Fix matching service bug whereby same user can be matched , revert timer to original values
    
    * ﻿Refactor Matching Service , Add Cancel Match Feature
    
    * Update all status in verifyToken to be UNAUTHORIZED
    
    * ﻿Fix matching service bug whereby same user can be matched , revert timer to original values
    
    Co-authored-by: figo2127 <61351392+figo2127@users.noreply.github.com>

--- matching-service/models/Interview.js
+++ matching-service/models/Interview.js
@@ -1,28 +1,23 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const interviewSchema = new mongoose.Schema({
-    interviewId: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    difficulty: {
-        type: String,
-        required: true
-    },
-    email1: {
-        type: String
-    },
-    email2: {
-        type: String
-    },
-    timeCreated: {
-        type: Date,
-        expires: 3600,
-        default: Date.now
-    }
+  interviewId: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  difficulty: {
+    type: String,
+    required: true,
+  },
+  email1: {
+    type: String,
+  },
+  email2: {
+    type: String,
+  },
 });
 
-const Interview = mongoose.model('InterviewModel', interviewSchema);
+const Interview = mongoose.model("InterviewModel", interviewSchema);
 
 module.exports = Interview;
