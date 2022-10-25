fd7ffaed9b2d79dafe231c824224adba94b602ba
figo2127 <61351392+figo2127@users.noreply.github.com>
2022-10-25 18:24:54 +0800

    run localhost if dev, else run cloud mongo

--- user-service/model/passwordToken-repository.js
+++ user-service/model/passwordToken-repository.js
@@ -7,7 +7,8 @@ import mongoose from 'mongoose';
 // let mongoDB = process.env.ENV == "PROD" ? process.env.DB_CLOUD_URI : process.env.DB_LOCAL_URI;
 const PASSWORD = process.env.PASSWORD
 const DB_NAME = process.env.DB_NAME
-const ATLAS_URI = `mongodb+srv://username:${PASSWORD}@peerprep-cluster.wcw5ljh.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`
+let ATLAS_URI = process.env.ENV == "DEV" ? process.env.DB_LOCAL_URI :
+    `mongodb+srv://username:${PASSWORD}@peerprep-cluster.wcw5ljh.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`
     || process.env.DB_LOCAL_URI;
 
 mongoose.connect(ATLAS_URI,
--- user-service/model/user-repository.js
+++ user-service/model/user-repository.js
@@ -8,8 +8,9 @@ import mongoose from 'mongoose';
 
 const PASSWORD = process.env.PASSWORD;
 const DB_NAME = process.env.DB_NAME;
-const ATLAS_URI = `mongodb+srv://username:${PASSWORD}@peerprep-cluster.wcw5ljh.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`
-|| process.env.DB_LOCAL_URI;
+let ATLAS_URI = process.env.ENV == "DEV" ? process.env.DB_LOCAL_URI :
+    `mongodb+srv://username:${PASSWORD}@peerprep-cluster.wcw5ljh.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`
+    || process.env.DB_LOCAL_URI;
 
 mongoose.connect(ATLAS_URI,
     { useNewUrlParser: true,
