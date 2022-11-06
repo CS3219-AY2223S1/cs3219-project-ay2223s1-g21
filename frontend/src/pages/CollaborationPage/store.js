0fef9b1e103a866e2ef3c71ea013fd3cbe896841
atmh <e0544426@u.nus.edu>
2022-11-06 11:43:40 +0800

    Add stun/turn server

--- frontend/src/pages/CollaborationPage/store.js
+++ frontend/src/pages/CollaborationPage/store.js
@@ -6,7 +6,23 @@ export const store = syncedStore({ collab: {}, fragment: "xml" });
 
 // Create a document that syncs automatically using Y-WebRTC
 const doc = getYjsValue(store);
-export const webrtcProvider = new WebrtcProvider("syncedstore-todos", doc);
+export const webrtcProvider = new WebrtcProvider("syncedstore-todos", doc, {
+    peerOpts: {
+        iceServers: [{
+            urls: [ "stun:ss-turn2.xirsys.com" ]
+        }, {
+            username: process.env.TURN_USERNAME,
+            credential: process.env.TURN_CREDENTIAL,
+            urls: [
+                "turn:ss-turn2.xirsys.com:80?transport=udp",
+                "turn:ss-turn2.xirsys.com:3478?transport=udp",
+                "turn:ss-turn2.xirsys.com:80?transport=tcp",
+                "turn:ss-turn2.xirsys.com:3478?transport=tcp",
+                "turns:ss-turn2.xirsys.com:443?transport=tcp",
+                "turns:ss-turn2.xirsys.com:5349?transport=tcp"
+            ]}]
+    }
+});
 
 export const disconnect = () => webrtcProvider.disconnect();
 export const connect = () => webrtcProvider.connect();
