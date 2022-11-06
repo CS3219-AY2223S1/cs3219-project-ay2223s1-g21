3962a05d6b1e4cf114db7eb32e7d085ddc28ffcb
tandeshao <e0544387@u.nus.edu>
2022-11-06 18:53:41 +0800

    add duck logo

--- frontend/src/pages/CollaborationPage/store.js
+++ frontend/src/pages/CollaborationPage/store.js
@@ -1,11 +1,11 @@
-import { syncedStore, getYjsValue } from "@syncedstore/core";
-import { WebrtcProvider } from "y-webrtc";
+import { syncedStore } from "@syncedstore/core";
+// import { WebrtcProvider } from "y-webrtc";
 
 // Create your SyncedStore store
 export const store = syncedStore({ collab: {}, fragment: "xml" });
 
 // Create a document that syncs automatically using Y-WebRTC
-const doc = getYjsValue(store);
+// const doc = getYjsValue(store);
 // export const webrtcProvider = new WebrtcProvider("syncedstore-todos", doc, {
 //     peerOpts: {
 //         iceServers: [{
