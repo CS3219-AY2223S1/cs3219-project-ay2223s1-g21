2f6cbbb1946221f4577a38cce834589d8cb85e95
tandeshao <e0544387@u.nus.edu>
2022-11-07 12:58:55 +0800

    remove voice

--- frontend/src/pages/CollaborationPage/index.js
+++ frontend/src/pages/CollaborationPage/index.js
@@ -34,7 +34,7 @@ import {
 import io from "socket.io-client";
 // import { connect, disconnect } from "./store";
 import { useNavigate } from "react-router-dom";
-import { Peer } from "peerjs";
+// import { Peer } from "peerjs";
 import { useSyncedStore } from "@syncedstore/react";
 import { store } from "./store";
 import { getYjsValue } from "@syncedstore/core";
@@ -51,7 +51,7 @@ export default function CollaborationPage() {
   const { isCodeRunning } = useSelector((state) => state.collabReducer);
   const { userId, jwtToken } = useSelector((state) => state.authReducer);
   const { roomId, difficulty } = useSelector((state) => state.matchingReducer);
-  const [peer, setPeer] = useState(false);
+  // const [peer, setPeer] = useState(false);
   const [ioSocket, setIoSocket] = useState(null);
   const [webRtc, setWebRtc] = useState(null);
 
@@ -142,55 +142,59 @@ export default function CollaborationPage() {
       setIoSocket(socket);
     });
 
-    const peer = new Peer(`${roomId}-${userId}`, { config: { iceServers: [{
-        urls: [ "stun:ss-turn2.xirsys.com" ]
-     }, {
-        username: "KPLaQwFEAY6GycEfX75qQuEgYo7UR6wA7V1Lxcsi-QVQWD5RZmXgEW0VnNI5ikc1AAAAAGNnKhJhbHZpbnRtaA==",
-        credential: "349d4154-5d83-11ed-854f-0242ac140004",
-        urls: [
-            "turn:ss-turn2.xirsys.com:80?transport=udp",
-            "turn:ss-turn2.xirsys.com:3478?transport=udp",
-            "turn:ss-turn2.xirsys.com:80?transport=tcp",
-            "turn:ss-turn2.xirsys.com:3478?transport=tcp",
-            "turns:ss-turn2.xirsys.com:443?transport=tcp",
-            "turns:ss-turn2.xirsys.com:5349?transport=tcp"
-        ]
-     }]}});
-    console.log("Peer Id :", peer.id);
-    setPeer(peer);
+    // const peer = new Peer(`${roomId}-${userId}`, { config: { iceServers: [{
+    //     urls: [ "stun:ss-turn2.xirsys.com" ]
+    //  }, {
+    //     username: "KPLaQwFEAY6GycEfX75qQuEgYo7UR6wA7V1Lxcsi-QVQWD5RZmXgEW0VnNI5ikc1AAAAAGNnKhJhbHZpbnRtaA==",
+    //     credential: "349d4154-5d83-11ed-854f-0242ac140004",
+    //     urls: [
+    //         "turn:ss-turn2.xirsys.com:80?transport=udp",
+    //         "turn:ss-turn2.xirsys.com:3478?transport=udp",
+    //         "turn:ss-turn2.xirsys.com:80?transport=tcp",
+    //         "turn:ss-turn2.xirsys.com:3478?transport=tcp",
+    //         "turns:ss-turn2.xirsys.com:443?transport=tcp",
+    //         "turns:ss-turn2.xirsys.com:5349?transport=tcp"
+    //     ]
+    //  }]}});
+    // console.log("Peer Id :", peer.id);
+    // setPeer(peer);
     const webrtcProvider = new WebrtcProvider(
       "peerprep-" + roomId,
       getYjsValue(store),
       {
         peerOpts: {
-            config: { 
-                iceServers: [{
-                    urls: [ "stun:ss-turn2.xirsys.com" ]
-                 }, {
-                    username: "KPLaQwFEAY6GycEfX75qQuEgYo7UR6wA7V1Lxcsi-QVQWD5RZmXgEW0VnNI5ikc1AAAAAGNnKhJhbHZpbnRtaA==",
-                    credential: "349d4154-5d83-11ed-854f-0242ac140004",
-                    urls: [
-                        "turn:ss-turn2.xirsys.com:80?transport=udp",
-                        "turn:ss-turn2.xirsys.com:3478?transport=udp",
-                        "turn:ss-turn2.xirsys.com:80?transport=tcp",
-                        "turn:ss-turn2.xirsys.com:3478?transport=tcp",
-                        "turns:ss-turn2.xirsys.com:443?transport=tcp",
-                        "turns:ss-turn2.xirsys.com:5349?transport=tcp"
-                    ]
-                 }]
-            }
-        }
-    }
+          config: {
+            iceServers: [
+              {
+                urls: ["stun:ss-turn2.xirsys.com"],
+              },
+              {
+                username:
+                  "KPLaQwFEAY6GycEfX75qQuEgYo7UR6wA7V1Lxcsi-QVQWD5RZmXgEW0VnNI5ikc1AAAAAGNnKhJhbHZpbnRtaA==",
+                credential: "349d4154-5d83-11ed-854f-0242ac140004",
+                urls: [
+                  "turn:ss-turn2.xirsys.com:80?transport=udp",
+                  "turn:ss-turn2.xirsys.com:3478?transport=udp",
+                  "turn:ss-turn2.xirsys.com:80?transport=tcp",
+                  "turn:ss-turn2.xirsys.com:3478?transport=tcp",
+                  "turns:ss-turn2.xirsys.com:443?transport=tcp",
+                  "turns:ss-turn2.xirsys.com:5349?transport=tcp",
+                ],
+              },
+            ],
+          },
+        },
+      }
     );
     setWebRtc(webrtcProvider);
 
-    webrtcProvider.on('synced', synced => {
+    webrtcProvider.on("synced", (synced) => {
       // NOTE: This is only called when a different browser connects to this client
       // Windows of the same browser communicate directly with each other
       // Although this behavior might be subject to change.
       // It is better not to expect a synced event when using y-webrtc
-      console.log('peers synced!', synced)
-    })
+      console.log("peers synced!", synced);
+    });
 
     // const webrtcProvider = new WebsocketProvider('wss://demos.yjs.dev',  "peerprep-" + roomId, getYjsValue(store))
     webrtcProvider.connect();
@@ -201,7 +205,7 @@ export default function CollaborationPage() {
       //socket io cleanup
       console.log("Disconnect Socket");
       socket.close();
-      peer.destroy();
+      // peer.destroy();
       webrtcProvider.disconnect();
     };
 
@@ -209,7 +213,6 @@ export default function CollaborationPage() {
       return shutDownProto;
     };
 
-    
     return shutDownProto;
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
@@ -218,109 +221,114 @@ export default function CollaborationPage() {
 
   const handleLogout = () => {
     dispatch(setIsLoading(true));
-    webRtc.disconnect()
+    webRtc.disconnect();
     dispatch(setIsLoading(false));
     dispatch(setLogout());
   };
 
-  useEffect(() => {
-    dispatch(setIsLoading(true));
-    if (ioSocket && !ioSocket.connected) {
-      webRtc.disconnect();
-      dispatch(setIsLoading(false));
-      navigate("/home");
-    }
-    
-    if (ioSocket && peer) {
-      ioSocket.emit("joinRoom", { roomId, jwtToken, userId });
-
-      peer.on("connection", () => {
-        console.log("Someone is connecting to me");
-      });
-
-      peer.on("call", (call) => {
-        navigator.mediaDevices
-          .getUserMedia({ video: false, audio: true })
-          .then((stream) => {
-            call.answer(stream); // Answer the call with an A/V stream.
-            call.on("stream", (remoteStream) => {
-              console.log("Got stream", remoteStream);
-              voiceChatRef.current.srcObject = remoteStream;
-              voiceChatRef.current.play();
-            });
-          })
-          .catch((err) => {
-            console.error("Failed to get local stream", err);
-          });
-      });
-
-      ioSocket.on("joinSuccessFirst", () => {
-        ioSocket.emit("TriggerFetchQn", {
-          roomId,
-          difficulty,
-          jwtToken,
-          userId,
-        });
-      });
-
-      ioSocket.on("joinSuccess", () => {
-        ioSocket.emit("startCall", {
-          peerid: peer.id,
-          roomId: roomId,
-          jwtToken,
-          userId,
-        });
-      });
-
-      ioSocket.on("recieveQn", (question) => {
-        console.log("Recieved Question");
-        dispatch(setQuestion(JSON.parse(question)));
-        updateHistory(userId, jwtToken, JSON.parse(question));
-        dispatch(setIsLoading(false));
-      });
-
-      ioSocket.on("callPeer", (peerId) => {
-        if (peerId !== peer.id) {
-          navigator.mediaDevices
-            .getUserMedia({ video: false, audio: true })
-            .then((stream) => {
-              console.log("New Call Request", peerId);
-              const call = peer.call(peerId, stream);
-              call.on("stream", (remoteStream) => {
-                console.log("Got stream", remoteStream);
-                voiceChatRef.current.srcObject = remoteStream;
-                voiceChatRef.current.play();
-              });
-            });
-        }
-      });
-
-      ioSocket.on("alreadyInRoom", () => {
-        console.log("Already in Room");
+  useEffect(
+    () => {
+      dispatch(setIsLoading(true));
+      if (ioSocket && !ioSocket.connected) {
         webRtc.disconnect();
         dispatch(setIsLoading(false));
         navigate("/home");
-      });
+      }
 
-      ioSocket.on("newChatMsg", (data) => {
-        console.log("Received new chat message", data);
-        const { userId: id, newMessage } = data;
-        if (id && id !== userId) {
-          addResponseMessage(newMessage);
-        }
-      });
+      // if (ioSocket && peer) {
+      if (ioSocket) {
+        ioSocket.emit("joinRoom", { roomId, jwtToken, userId });
 
-      ioSocket.on("leaveRoom", () => {
-        alert("A participant has left the room");
-      });
+        // peer.on("connection", () => {
+        //   console.log("Someone is connecting to me");
+        // });
 
-      ioSocket.on("badRequest", () => {
-        dispatch(setIsLoading(false));
-        navigate("/home");
-      });
-    }
+        // peer.on("call", (call) => {
+        //   navigator.mediaDevices
+        //     .getUserMedia({ video: false, audio: true })
+        //     .then((stream) => {
+        //       call.answer(stream); // Answer the call with an A/V stream.
+        //       call.on("stream", (remoteStream) => {
+        //         console.log("Got stream", remoteStream);
+        //         voiceChatRef.current.srcObject = remoteStream;
+        //         voiceChatRef.current.play();
+        //       });
+        //     })
+        //     .catch((err) => {
+        //       console.error("Failed to get local stream", err);
+        //     });
+        // });
+
+        ioSocket.on("joinSuccessFirst", () => {
+          ioSocket.emit("TriggerFetchQn", {
+            roomId,
+            difficulty,
+            jwtToken,
+            userId,
+          });
+        });
+
+        ioSocket.on("joinSuccess", () => {
+          ioSocket.emit("startCall", {
+            // peerid: peer.id,
+            roomId: roomId,
+            jwtToken,
+            userId,
+          });
+        });
+
+        ioSocket.on("recieveQn", (question) => {
+          console.log("Recieved Question");
+          dispatch(setQuestion(JSON.parse(question)));
+          updateHistory(userId, jwtToken, JSON.parse(question));
+          dispatch(setIsLoading(false));
+        });
+
+        // ioSocket.on("callPeer", (peerId) => {
+        //   if (peerId !== peer.id) {
+        //     navigator.mediaDevices
+        //       .getUserMedia({ video: false, audio: true })
+        //       .then((stream) => {
+        //         console.log("New Call Request", peerId);
+        //         const call = peer.call(peerId, stream);
+        //         call.on("stream", (remoteStream) => {
+        //           console.log("Got stream", remoteStream);
+        //           voiceChatRef.current.srcObject = remoteStream;
+        //           voiceChatRef.current.play();
+        //         });
+        //       });
+        //   }
+        // });
+
+        ioSocket.on("alreadyInRoom", () => {
+          console.log("Already in Room");
+          webRtc.disconnect();
+          dispatch(setIsLoading(false));
+          navigate("/home");
+        });
+
+        ioSocket.on("newChatMsg", (data) => {
+          console.log("Received new chat message", data);
+          const { userId: id, newMessage } = data;
+          if (id && id !== userId) {
+            addResponseMessage(newMessage);
+          }
+        });
+
+        ioSocket.on("leaveRoom", () => {
+          alert("A participant has left the room");
+        });
+
+        ioSocket.on("badRequest", () => {
+          dispatch(setIsLoading(false));
+          navigate("/home");
+        });
+      }
+    },
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [ioSocket, peer]);
+    //}, [ioSocket, peer]);
+    [ioSocket]
+  );
 
   const handleNewUserMessage = (newMessage) => {
     ioSocket.emit("sendChatMsg", {
